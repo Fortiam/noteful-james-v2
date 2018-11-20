3b7d408290fa1d400fa1f30821f4fe11081694f3
James Bell <Noxachron@gmail.com>
2018-11-20 16:19:49 -0500

    added knex routing to postgresQL

--- routes/notes.js
+++ routes/notes.js
@@ -6,44 +6,63 @@ const express = require('express');
 const router = express.Router();
 
 // TEMP: Simple In-Memory Database
-const data = require('../db/notes');
-const simDB = require('../db/simDB');
-const notes = simDB.initialize(data);
+// const data = require('../db/notes');
+// const simDB = require('../db/simDB');
+// const notes = simDB.initialize(data);
+const knex = require('../knex');
 
 // Get All (and search by query)
 router.get('/', (req, res, next) => {
-  const { searchTerm } = req.query;
-
-  notes.filter(searchTerm)
-    .then(list => {
-      res.json(list);
-    })
-    .catch(err => {
-      next(err);
-    });
+  const searchTerm = req.query.searchTerm;
+  knex
+  .select('id', 'title', 'content')
+  .from('notes')
+  .modify(queryBuilder => {
+    if (searchTerm) {
+      queryBuilder.where('title', 'like', `%${searchTerm}%`);
+    }
+  })
+  .orderBy('id')
+  .then(results => {
+    //results.status(200);
+    res.json(results);
+  })
+  .catch(err => {
+    next(err);
+  });
 });
 
 // Get a single item
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
 
-  notes.find(id)
-    .then(item => {
-      if (item) {
-        res.json(item);
-      } else {
-        next();
-      }
-    })
-    .catch(err => {
+  knex
+  .select()
+  .from('notes')
+  .where('id', id)
+  .returning('id', 'title', 'content')
+  .then(function(results) { 
+    if(results.length === 0){
+      const err = new Error('id Not Found');
+      err.status = 404;
       next(err);
-    });
+    }
+    res.json(results[0]);
+  })
+  .catch(function(err){
+    err.status = 404;
+    next(err);});
+
 });
 
 // Put update an item
 router.put('/:id', (req, res, next) => {
   const id = req.params.id;
-
+  if(!id){
+    const err = new Error('Invalid `id`');
+    err.status = 400;
+    return next(err);
+  }
   /***** Never trust users - validate input *****/
   const updateObj = {};
   const updateableFields = ['title', 'content'];
@@ -55,23 +74,18 @@ router.put('/:id', (req, res, next) => {
   });
 
   /***** Never trust users - validate input *****/
-  if (!updateObj.title) {
-    const err = new Error('Missing `title` in request body');
-    err.status = 400;
-    return next(err);
-  }
-
-  notes.update(id, updateObj)
-    .then(item => {
-      if (item) {
-        res.json(item);
-      } else {
-        next();
-      }
-    })
-    .catch(err => {
-      next(err);
-    });
+  // if (!updateObj.title) {
+  //   const err = new Error('Missing `title` in request body');
+  //   err.status = 400;
+  //   return next(err);
+  // }
+  knex
+  .from('notes')
+  .where('id', id)
+  .update(updateObj)
+  .then(results => res.json(results))
+  .catch(err => next(err));
+  
 });
 
 // Post (insert) an item
@@ -85,29 +99,25 @@ router.post('/', (req, res, next) => {
     err.status = 400;
     return next(err);
   }
-
-  notes.create(newItem)
-    .then(item => {
-      if (item) {
-        res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
-      }
-    })
-    .catch(err => {
-      next(err);
-    });
+  knex
+  .from('notes')
+  .insert(newItem)
+  .returning('id', 'name', 'title')
+  .then(results=> res.location(`http://${req.headers.host}/notes/${results.id}`).status(201).json(results))
+  .catch(err=> next(err));
+  
 });
 
 // Delete an item
 router.delete('/:id', (req, res, next) => {
   const id = req.params.id;
-
-  notes.delete(id)
-    .then(() => {
-      res.sendStatus(204);
-    })
-    .catch(err => {
-      next(err);
-    });
+  knex
+  .from('notes')
+  .where('id', id)
+  .del()
+  .then(results=> res.status(204).end())
+  .catch(err=> next(err));
+  
 });
 
 module.exports = router;
--- scratch/queries.js
+++ scratch/queries.js
@@ -2,7 +2,7 @@
 
 const knex = require('../knex');
 
-let searchTerm = 'gaga';
+let searchTerm = req.query;
 knex
   .select('notes.id', 'title', 'content')
   .from('notes')
@@ -13,8 +13,46 @@ knex
   })
   .orderBy('notes.id')
   .then(results => {
-    console.log(JSON.stringify(results, null, 2));
+    JSON.stringify(results);
   })
   .catch(err => {
-    console.error(err);
+    next(err);
   });
+
+let myId = req.params.id;
+  knex
+    .select()
+    .from('notes')
+    .where('id', myId)
+    .returning('id', 'title', 'content')
+    .then(results => JSON.stringify(results))
+    .catch(err => next(err));
+
+let userID = req.params.id;
+let updateObj = req.body;
+  knex
+    .from('notes')
+    .where('id', userID)
+    .update(updateObj)
+    .then(results => res.json(results))
+    .catch(err => next(err));
+
+let myObj = req.body;
+knex
+  .from('notes')
+  .insert(myObj)
+  .returning('id', 'name', 'title')
+  .then(results=> res.send(results))
+  .catch(err=> next(err));
+
+let IdtoDelete = req.params.id;
+knex
+  .from('notes')
+  .where('id', IdtoDelete)
+  .del()
+  .then(results=> res.json(results))
+  .catch(err=> next(err));
+
+
+
+
